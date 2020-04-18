a747a79ee8ca83e5d44babafd945312cc73afb7d
Faber <faberhozzherrera@gmail.com>
2020-04-18 18:41:57 -0500

    add menu component

--- src/app/accordeon/accordeon.component.ts
+++ src/app/accordeon/accordeon.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-accordeon',
   templateUrl: './accordeon.component.html',
-  styleUrls: ['./accordeon.component.sass']
+  styleUrls: ['./accordeon.component.scss']
 })
 export class AccordeonComponent implements OnInit {
 
--- src/app/submenu/submenu.component.ts
+++ src/app/submenu/submenu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-submenu',
   templateUrl: './submenu.component.html',
-  styleUrls: ['./submenu.component.sass']
+  styleUrls: ['./submenu.component.scss']
 })
 export class SubmenuComponent implements OnInit {
 
