fce05d1ef6a952df2a8a779cd8eec09b99879c7c
Faber <faberhozzherrera@gmail.com>
2020-04-18 17:43:24 -0500

    project estucture

---
+++ src/app/accordeon/accordeon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accordeon',
+  templateUrl: './accordeon.component.html',
+  styleUrls: ['./accordeon.component.sass']
+})
+export class AccordeonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SubmenuComponent } from './submenu/submenu.component';
+import { InformationComponent } from './information/information.component';
+import { AccordeonComponent } from './accordeon/accordeon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SubmenuComponent,
+    InformationComponent,
+    AccordeonComponent
   ],
   imports: [
     BrowserModule,
