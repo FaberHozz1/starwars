ac7a2e1402709c47c11ae5677f0e9a7f3b49c9f2
cguasca2019 <51269156+cguasca2019@users.noreply.github.com>
2020-04-21 21:36:27 -0500

    Acordeon acoplado, dar NPM INSTALL

--- src/app/accordeon/accordeon.component.ts
+++ src/app/accordeon/accordeon.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordeonComponent implements OnInit {
 
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/nodata/nodata.component.ts
+++ src/app/nodata/nodata.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nodata',
   templateUrl: './nodata.component.html',
-  styleUrls: ['./nodata.component.sass']
+  styleUrls: ['./nodata.component.scss']
 })
 export class NodataComponent implements OnInit {
 
