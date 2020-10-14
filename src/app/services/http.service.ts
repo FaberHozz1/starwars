b142962dbc3d28aeaf3b53fbd3c092ecc0d7f7aa
FaberHozz1 <faberhozzherrera@gmail.com>
2020-10-13 21:53:19 -0500

    endpoint

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { URLS } from '../const/urls';
 import { Observable } from 'rxjs';
 
 @Injectable()
